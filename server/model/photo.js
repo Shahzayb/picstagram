59563cc522facee356cd9c54a4db108f786f3a5c
Shahzaib Sarwar <imshahzayb@gmail.com>
2020-02-06 10:11:50 +0500

    implemented 'get /api/timeline' route | Fixes #46 (#47)
    Timeline wouldn't include owner's own photos.

--- server/model/photo.js
+++ server/model/photo.js
@@ -1,32 +1,35 @@
 const mongoose = require('mongoose');
 
-const PhotoSchema = new mongoose.Schema({
-  photoUrl: {
-    type: String,
-    required: true
-  },
-  tags: [
-    {
-      type: String
+const PhotoSchema = new mongoose.Schema(
+  {
+    photoUrl: {
+      type: String,
+      required: true
+    },
+    tags: [
+      {
+        type: String
+      }
+    ],
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'user',
+      required: true
+    },
+    title: {
+      type: String,
+      default: '',
+      required: true,
+      maxlength: 120
+    },
+    likeCount: {
+      type: Number,
+      default: 0,
+      required: true
     }
-  ],
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'user',
-    required: true
   },
-  title: {
-    type: String,
-    default: '',
-    required: true,
-    maxlength: 120
-  },
-  likeCount: {
-    type: Number,
-    default: 0,
-    required: true
-  }
-});
+  { timestamps: true }
+);
 
 const Photo = mongoose.model('photo', PhotoSchema);
 
